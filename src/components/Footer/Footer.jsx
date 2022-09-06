f7cb014cc42ff27441ca2972e3e7126077e59fd2
Erika <erikaladner5@gmail.com>
2022-09-05 21:51:51 -0300

    Todo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom";
+import { FaGithub } from 'react-icons/fa';
 import './Footer.css';
 
 export default function Footer(){
@@ -12,7 +13,10 @@ export default function Footer(){
                 <div class="col-12 col-lg">
                 <Link to="/contactarme">Contactarme</Link>
                 </div>
+                <div className="git">
                 <div className="yo">by Erika Ladner</div>
+                <a href="https://github.com/erikalad/react-redux"><FaGithub/></a>
+                </div>
             </div>
     </footer>
     )
